f7fe5c3ed8c4030b1ca468a1006fa64673975cbd
RihardsJ <rihards.jukna@gmail.com>
2020-12-07 10:09:58 +0000

    remove output test, fix typos, define functions output test will be integrated in each function test defined functions and their I/O
    Relates #1

--- tests/primeNumbers.test.js
+++ tests/primeNumbers.test.js
@@ -2,5 +2,5 @@ const primeNumbers = require("../assets/primeNumbers");
 
 // test if function exists
 test("primeNumbers function to be defined", () => {
-  expect(primeNumbers).toBeDefinded();
+  expect(primeNumbers()).toBeDefined();
 });
