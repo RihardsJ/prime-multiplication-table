c14209aa673baef62417825a262e490a2ce159ae
RihardsJ <rihards.jukna@gmail.com>
2020-12-07 13:10:49 +0000

    build five unit tests for inputNumber function

--- tests/getInputNumber.test.js
+++ tests/getInputNumber.test.js
@@ -1,6 +1,30 @@
-const getInputNumber = require("../assets/getInputNumber");
+const getInputNumber = require("../assets/getInputNumer");
 
-// test if function exists
-test("getInputNumber function to be defined", () => {
-  expect(getInputNumber).toBeDefinded();
+describe("Input Number tests: ", () => {
+  // test if function exists
+  test("getInputNumber function  be defined", () => {
+    expect(getInputNumber()).toBeDefined();
+  });
+
+  test("numeric value of input should not be changed", () => {
+    expect(getInputNumber(55).toEqual(55));
+  });
+
+  test("should recieve a string integer and return number", () => {
+    expect(getInputNumber("10")).toEqual(10);
+  });
+
+  test("input should be round down nearest integer", () => {
+    expoct(getInputNumber(21.1234).toEqual(21));
+  });
+
+  test("input should be positive number", () => {
+    expect(getInputNumber("eleven")).toThrowError(
+      "Error! Number Cannot be negative! Please insert positive integer."
+    );
+  });
+
+  test("input should be positive number", () => {
+    expect(getInputNumber(-20)).toThrowErro("Error! Should should be bigger than 1");
+  });
 });
